7ff8a72a6dc0bef6ef2d68af8c86d7d41706b0ba
li <li@lideMacBook-Air.local>
2022-11-02 18:50:11 -0400

    save

--- cli.js
+++ cli.js
@@ -1,68 +1,64 @@
 #!/usr/bin/env node
-//import moment, minimist and fetch
-import moment from "moment-timezone"
-import minimist from "minimist"
-import fetch from "node-fetch"
 
-const args = minimist(process.argv.slice(2))
-//set up Time zone: uses tz.guess() from moment-timezone by default.
-let timezone = moment.tz.guess()
-if (args.t) {
-    timezone = args.t
-}
-//set up -h, Show this help message and exit.
-if (args.h) {
-    console.log(`
-        Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+import minimist from "minimist";
+
+import fetch from "node-fetch";
+
+import moment from "moment-timezone";
+
+const args = minimist(process.argv.splice(2));
+
+if(args.h) {
+    console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
         -h            Show this help message and exit.
         -n, -s        Latitude: N positive; S negative.
         -e, -w        Longitude: E positive; W negative.
-        -t            Time zone: uses tz.guess() from moment-timezone by default.
+        -z            Time zone: uses tz.guess() from moment-timezone by default.
         -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
         -j            Echo pretty JSON from open-meteo API and exit.
-        `)
+    `);
+    process.exit(0);
+}
+
+let latitude = 0;
+let longitude = 0;
+if(args.n) {
+    latitude = args.n;
+} else if (args.s) {
+    latitude = -args.s;
 } else {
-    //set up Latitude and Longitude
-    //default latitude
-    let latitude = '35'
-    if(args.n) {
-        latitude = args.n
-    }
+    console.log('Invalid Latitude');
+}
+if(args.e) {
+    longitude = args.e;
+} else if (args.w) {
+    longitude = -args.w;
+} else {
+    console.log('Invalid Longitude');
+}
 
-    if(args.s) {
-        latitude = args.s
-    }
-    //default longitude
-    let longitude = '79'
-    if (args.e) {
-        longitude = args.e
-    }
-    if (args.w) {
-        longitude = args.w
-    }
-    //output time
-    const days = args.d
+let timezone = moment.tz.guess();
+if(args.z) {
+    timezone = args.z;
+}
 
-    if (days == 0) {
-        console.log("today.")
-    } else if (days > 1) {
-        console.log("in " + days + " days.")
-    } else {
-        console.log("tomorrow.")
-    }
-    //fetch api
-    const response = await fetch(
-        'https://api.open-meteo.com/v1/forecast?latitude='
-        + latitude
-        + '&longitude='
-        + longitude
-        + '&daily=weathercode,temperature_2m_max,sunrise,precipitation_sum,precipitation_hours&timezone='
-        + timezone)
-    //json output
-    const data = await response.json()
-    //print output
-    console.log(data)
+let day = 1;
+if(args.d || args.d==0) {
+    day = args.d;
+}
 
+const response = await fetch(
+    'https://api.open-meteo.com/v1/forecast?latitude='
+    + latitude
+    + '&longitude='
+    + longitude +
+    '&daily=precipitation_hours&timezone='
+    + timezone);
+
+const data = await response.json();
+
+if(args.j) {
+    console.log(data);
 }
 
 let result = ''
@@ -70,6 +66,14 @@ if(data.daily.precipitation_hours[day] != 0){
     result = 'You might need your galoshes ';
 } else {
     result = "You probably won't need your galoshes ";
+}
+
+if (day == 0) {
+    result += "today.";
+} else if (day > 1) {
+    result += "in " + day + " days.";
+} else {
+    result += "tomorrow.";
 }
 
 console.log(result);
